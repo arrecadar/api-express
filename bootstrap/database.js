19772aa5a368cd7800e61c3ee62ea6cf1a30f610
uselessdev <wallacebatistaoliveira@gmail.com>
2018-04-18 23:01:50 -0300

    database: add plugins to mongoose

--- bootstrap/database.js
+++ bootstrap/database.js
@@ -1,4 +1,12 @@
 const mongoose = require('@anarklab/expressive-mongodb')
+const clean = require('@teamsight/mongoose-clean')
+const paginate = require('mongoose-paginate')
 const { MONGODB } = require('../config/database')
 
-module.exports = mongoose(MONGODB)
+const database = mongoose(MONGODB)
+
+// define globals plugins
+database.plugin(clean)
+database.plugin(paginate)
+
+module.exports = database
