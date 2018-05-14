e636b456178d3e942671e77c3290f7fe5eef1960
Lucas Pires <lucas.pires.mattos@gmail.com>
2018-05-14 00:35:28 -0300

    development-environment: add database variables

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
 module.exports = {
-  MONGODB: process.env.MONGODB_URI || 'localhost:3333'
+  DB_CONNECTION: process.env.DB_CONNECTION,
+  DB_HOSTNAME: process.env.DB_HOSTNAME,
+  DB_PORT: process.env.DB_PORT,
+  DB_NAME: process.env.DB_NAME,
+  DB_USERNAME: process.env.DB_USERNAME,
+  DB_PASSWORD: process.env.DB_PASSWORD
 }
