f46b1091dcd97ad299b096eab9632b3fcdb763bf
Wallace Batista <wallacebatistaoliveira@gmail.com>
2018-05-14 18:35:19 -0300

    Revert "Docker development environment"

--- bootstrap/database.js
+++ bootstrap/database.js
@@ -1,17 +1,9 @@
 const mongoose = require('@anarklab/expressive-mongodb')
 const clean = require('@teamsight/mongoose-clean')
 const paginate = require('mongoose-paginate')
-const {
-  DB_CONNECTION,
-  DB_HOSTNAME,
-  DB_PORT,
-  DB_NAME,
-  DB_USERNAME,
-  DB_PASSWORD
-} = require('../config/database')
+const { MONGODB } = require('../config/database')
 
-const connection = `${DB_CONNECTION}://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOSTNAME}:${DB_PORT}/${DB_NAME}`
-const database = mongoose(connection)
+const database = mongoose(MONGODB)
 
 // define globals plugins
 database.plugin(clean)
--- config/database.js
+++ config/database.js
@@ -1,8 +1,3 @@
 module.exports = {
-  DB_CONNECTION: process.env.DB_CONNECTION,
-  DB_HOSTNAME: process.env.DB_HOSTNAME,
-  DB_PORT: process.env.DB_PORT,
-  DB_NAME: process.env.DB_NAME,
-  DB_USERNAME: process.env.DB_USERNAME,
-  DB_PASSWORD: process.env.DB_PASSWORD
+  MONGODB: process.env.MONGODB_URI || 'localhost:3333'
 }
