5684d0fba8f0ac896a9ed5302ce1cab23aba6d04
Lucas Pires <lucas.pires.mattos@gmail.com>
2018-06-03 22:05:02 -0300

    docker: add service for test suites

---
+++ tests/integration/example.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai')
+
+describe('Example', function () {
+  it('should be false', function () {
+    expect(false).to.equal(false)
+  })
+})
---
+++ tests/unit/example.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai')
+
+describe('Example', function () {
+  it('should be true', function () {
+    expect(true).to.equal(true)
+  })
+})
